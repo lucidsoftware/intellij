5983e638da7c1516fb81af51d442c41d2a60cc8e
Corey Woodfield <cwoodfield@lucidchart.com>
2021-11-18 16:17:59 -0700

    Mark all available scala SDKs as scala SDKs

--- scala/src/com/google/idea/blaze/scala/sync/BlazeScalaSyncPlugin.java
+++ scala/src/com/google/idea/blaze/scala/sync/BlazeScalaSyncPlugin.java
@@ -111,7 +111,6 @@ public class BlazeScalaSyncPlugin implements BlazeSyncPlugin {
         Option<String> version = matcher.find() ? Some.apply(matcher.group()) : Option$.MODULE$.empty();
         editor.setProperties(ScalaLibraryProperties.apply(version, Seq$.MODULE$.empty()));
         editor.commit();
-        return;
       }
     }
   }
