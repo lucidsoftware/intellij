f53f451ea7d82ec23945fcc7df6e6d2529430979
Brian Dong <bdong@lucidchart.com>
2024-12-02 14:21:41 -0700

    Patch plugin to use local execution by default for test debugging

--- java/src/com/google/idea/blaze/java/run/BlazeJavaRunProfileState.java
+++ java/src/com/google/idea/blaze/java/run/BlazeJavaRunProfileState.java
@@ -291,6 +291,7 @@ public final class BlazeJavaRunProfileState extends BlazeJavaDebuggableRunProfil
       }
       else {
         command.addBlazeFlags(BlazeFlags.JAVA_TEST_DEBUG);
+        command.addBlazeFlags(BlazeFlags.RUN_LOCALLY);
         command.addBlazeFlags(debugPortFlag(true, debugPort));
       }
       if (kotlinxCoroutinesJavaAgent != null) {
